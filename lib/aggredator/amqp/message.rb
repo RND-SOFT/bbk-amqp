5bf16d0b7454af3edc2e594597b5800e418e09fa
Samoilenko Yuri <kinnalru@gmail.com>
2020-10-21 14:59:00 +0300

    add with_indifferent_access

--- lib/aggredator/amqp/message.rb
+++ lib/aggredator/amqp/message.rb
@@ -12,11 +12,11 @@ module Aggredator
         @properties = properties.to_h.with_indifferent_access
         @body = body
         @delivery_info = delivery_info.to_h.merge(message_consumer: consumer, protocols: consumer.protocols)
-        @headers = @properties.except(:headers).merge(properties[:headers])
+        @headers = @properties.except(:headers).merge(properties[:headers]).with_indifferent_access
         @payload = begin
-                     JSON.parse(body)
+                     JSON.parse(body).with_indifferent_access
                    rescue StandardError
-                     {}
+                     {}.with_indifferent_access
                    end
       end
 
